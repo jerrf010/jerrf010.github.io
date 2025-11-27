a1afe26d22ba981dd1573bb0f26f0e27724df7cb
jerrf010 <huntingaiden513@gmail.com>
2025-11-27 14:16:52 -0500

    Update

---
+++ Javascript/Lesson 3/quadrants.js
@@ -0,0 +1,18 @@
+let x = Number(prompt());
+let y = Number(prompt());
+
+if (x > 0 && y > 0) {
+	console.log("Quadrant 1");
+}
+
+else if (x < 0 && y > 0) {
+	console.log("Quadrant 2");
+}
+
+else if (x < 0 && y < 0) {
+	console.log("Quadrant 3");
+}
+
+else {
+	console.log("Quadrant 4");
+}
